63955270fd3153076a1b73698c406e7ba6874084
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-07 23:46:17 +0530

    Update Auth.jsx

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -4,7 +4,7 @@ const Auth = () =>{
     return(
         <div>
             <h1>Auth Page</h1>
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti animi voluptates nam et! Non inventore aspernatur distinctio sunt, a odit dolorem, consectetur et maxime dolor voluptatem incidunt est? Nobis, eaque?</h1>
+            <h1>Lorem ipsum olor sit amet consectetur, adipisicing elit. Corrupti animi voluptates nam et! Non inventore aspernatur distinctio sunt, a odit dolorem, consectetur et maxime dolor voluptatem incidunt est? Nobis, eaque?</h1>
         </div>
     )
 }
