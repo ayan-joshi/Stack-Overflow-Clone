663ed7f8d04a3f7201a192cdfc95b8d524466f77
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-08 20:10:05 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,16 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import AllRoutes from "./AllRoutes";
-import { fetchAllQuestions } from "./actions/question";
-import { fetchAllUsers } from "./actions/users";
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import AllRoutes from './AllRoutes'
+
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchAllQuestions());
-    dispatch(fetchAllUsers());
-  }, [dispatch]);
-
-  const [slideIn, setSlideIn] = useState(true);
-
-  useEffect(() => {
-    if (window.innerWidth <= 760) {
-      setSlideIn(false);
-    }
-  }, []);
-
-  const handleSlideIn = () => {
-    if (window.innerWidth <= 760) {
-      setSlideIn((state) => !state);
-    }
-  };
-
   return (
     <div className="App">
       <Router>
-        <Navbar handleSlideIn={handleSlideIn} />
-        <AllRoutes slideIn={slideIn} handleSlideIn={handleSlideIn} />
+      <Navbar/>
+      <AllRoutes/>
       </Router>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import Reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const store = createStore(Reducers, compose(applyMiddleware(thunk)));
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
