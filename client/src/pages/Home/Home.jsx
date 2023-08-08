15b41ff2a6789a85d4c01e0381ab1aa3973be2f0
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-08 18:23:51 +0530

    com

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Home = () =>{
-    return(
-        <div>
+import "../../App.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
+import HomeMainbar from "../../components/HomeMainbar/HomeMainbar";
 
-        </div>
-    )
-}
+const Home = ({ slideIn }) => {
+  return (
+    <div className="home-container-1">
+      <LeftSidebar slideIn={slideIn} />
+      <div className="home-container-2">
+        <HomeMainbar />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
