a32920a61a189b2fc3ad86699b1bdaf0266a3480
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-09 12:29:04 +0530

    commit

---
+++ client/src/pages/Auth/AboutAuth.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AboutAuth = () =>{
+    return(
+        <div className="auth-container-1">
+            <h1>Join the Stack Overflow community</h1>
+            <p>Get unstuck - ask a question</p>
+            <p>Unlock new previleges like voting and commenting</p>
+            <p>Save your favorite tahs, filters and jobs</p>
+            <p>Earn reputation and badges</p>
+            <p style={{color: "#666767", fontsize:'13px'}}>Collaborate and share knowledge with a private group</p>
+            <p style={{color:"#007ac6", fontsize:'13px'}}>Get Stack Overflow for Teams free for up to 50 users</p>
+
+        </div>
+    )
+}
+
+export default AboutAuth
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Auth.css'
+import icon from '../../assets/icon.png'
+import AboutAuth from './AboutAuth'
+
 
 const Auth = () =>{
+    const [isSignup, setIsSignup] = useState(false)
+
+    const handleSwitch = () =>{
+        setIsSignup(!isSignup)
+    }
     return(
-        <div>
-            <h1>Auth Page</h1>
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti animi voluptates nam et! Non inventore aspernatur distinctio sunt, a odit dolorem, consectetur et maxime dolor voluptatem incidunt est? Nobis, eaque?</h1>
-        </div>
+        <section class='auth-section'>
+            { isSignup && <AboutAuth/>}
+            <div class='auth-container'>
+                { !isSignup && <img src={icon} alt='stack overflow' className='login-logo'/>}
+                <form>
+                    {
+                        isSignup && (
+                            <label htmlFor='name'>
+                                <h4>Display Name</h4>
+                                <input type='text' id='name' name='name'/>
+                            </label>
+                        )
+                    }
+                    <label htmlFor="email">
+                        <h4>Email</h4>
+                        <input type="email" name='name' id='email'/>
+                    </label>
+                    <label htmlFor="password">
+                        <div>
+                        <h4>Password</h4>
+                        { !isSignup && <p  style={{color:"#007ac6"}}>forgot password?</p> }
+                        </div>
+                        <input type="password" name='password' id='password'/>
+                        { isSignup && <p style={{fontsize:'13px'}}>Password must contain at least eight<br/> characters, including at least 1 letter and 1,<br/> number.</p>}
+                    </label>
+                    {
+                        isSignup && (
+                            <label htmlFor='check'>
+                                <input type='checkbox' id='check'/>
+                                <p style={{fontsize:'13px'}}>Opt-in to receive occaisonal,<br/>product updates, user research invitations,<br/>company announcements, and digests.</p>
+                            </label>
+                        )
+                    }
+                    <button type='submit' className='auth-btn'>{ isSignup ? 'Sign up': 'Log in'} </button>
+                    {
+                        isSignup && (
+                            <p style={{color: "#666767", fontsize:'13px'}}>By clicking “Sign up”, you agree to our
+                            <span style={{color:"#007ac6"}}> terms of<br/> service</span>,
+                            <span style={{color:"#007ac6"}}> privacy policy</span> and
+                            <span style={{color:"#007ac6"}}> cookie policy</span>
+                            </p>
+                        )
+                    }
+                </form>
+                <p>
+                    {isSignup ? 'Already have an account?': "Don't have an account?"}
+                    <button type='button' className='handle-switch-btn' onClick={handleSwitch}>{ isSignup ? "Log in" : 'sign up' }</button>
+                </p>
+            </div>
+
+        </section>
     )
 }
 
