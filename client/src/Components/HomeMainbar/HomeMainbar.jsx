419379cadda3561cdd5ce0cefcd88b934651e434
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-09 20:19:37 +0530

    commit

--- client/src/Components/HomeMainbar/HomeMainbar.jsx
+++ client/src/Components/HomeMainbar/HomeMainbar.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import './HomeMainbar.css'
+import {Link , useLocation} from 'react-router-dom'
+import Questions from './Questions'
+import QuestionList from './QuestionList'
 
-const HomeMainbar = () =>{
+
+const HomeMainbar = () =>{ 
+
+    var questionList = [{
+        id: 1,
+        votes: 3,
+        noOfAnswers: 2,
+        questionTitle: "What is a function",
+        questionBody: "It meant to be",
+        questionTags: ["java","node js","mongo","react js"],
+        userPosted: "mano",
+        askedOn:"jan 1"
+    },{
+        id: 2,
+        votes: 0,
+        noOfAnswers: 0,
+        questionTitle: "What is a function",
+        questionBody: "It meant to be",
+        questionTags: ["javascript","python","R"],
+        userPosted: "mano",
+        askedOn:"jan 1"
+    },{
+        id: 3,
+        votes: 0,
+        noOfAnswers: 0,
+        questionTitle: "What is a function",
+        questionBody: "It meant to be",
+        questionTags: ["java","node js","mongo","c++"],
+        userPosted: "mano",
+        askedOn:"jan 1"
+    }]
+const location = useLocation()
     return(
-        <div>
-
+        <div className='main-bar'>
+            <div className='main-bar-header'>
+                 {
+                    location.pathname === '/' ? <h1>Top Questions</h1> : <h1>All Questions</h1>
+                 }
+                 <Link to='/AskQuestion' className='ask-btn'>Ask Questions</Link>       
+            </div>
+            <div>
+                {
+                     questionList === null ?(
+                     <h1>Loading...</h1>
+    ):(
+                     <>
+                     <p>{ questionList.length } questions</p>
+                     <QuestionList questionList={questionList} />
+                     </>
+                )}
+               
+            </div>
         </div>
     )
 }
