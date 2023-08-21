a04bad1242ee9c169a975990afcd67ec8f768a5c
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-21 23:30:42 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(Reducers, compose(Middleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
--- server/models/auth.js
+++ server/models/auth.js
@@ -4,7 +4,9 @@ const userSchema = mongoose.Schema({
     name: {type: String, requires: true},
     email: {type: String, required: true},
     password: {type : String, required:true},
-    about: {type : String, required:true},
-    tags: {type : String, required:true},
-    joinedOn: {type : String, required:true}
+    about: {type : String},
+    tags: {type : [String]},
+    joinedOn: {type : Date, default: Date.now}
 })
+
+export default mongoose.model("User", userSchema)
