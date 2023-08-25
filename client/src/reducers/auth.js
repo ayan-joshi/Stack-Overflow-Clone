6e9ba8e833040b5ea0cb55ef127c517fea1c2a31
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-25 23:39:02 +0530

    c

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,10 @@
+const authReducer = (state= {data:null}, actions) => {
+    switch(actions.type){
+        case 'AUTH':
+            return state
+        default:
+            return state
+    }    
+}
+
+export default authReducer
