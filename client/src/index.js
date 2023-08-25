f6c40dce02f46fba2192c3ee6ca86a32587a8d06
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-08-25 22:57:13 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
+import { createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
+import Reducers from './reducers'
 
-const store = createStore(Reducers, compose(Middleware(thunk)))
+
+const store = createStore(Reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
