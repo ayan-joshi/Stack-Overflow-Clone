b9b8b0495f64c419a86753b0f8b930eaaedc0126
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-09-05 02:13:03 +0530

    comm

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,11 +3,20 @@ import * as api from '../api'
 
 export const signup = (authData, navigate) => async (dispatch) => {
     try{
-        const { data} = await api.signup(authData)
+        const { data} = await api.signUp(authData)
         dispatch({ type: 'AUTH', data})
+        navigate('/')
     } catch(error){
-
+        console.log(error)
     }
 }
 
-export const login = (authData, navigate) => {}
+export const login = (authData, navigate) => async (dispatch) => {
+    try{
+        const { data} = await api.logIn(authData)
+        dispatch({ type: 'AUTH', data})
+        navigate('/')
+    } catch(error){
+        console.log(error)
+    }
+}
